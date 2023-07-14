ecd698e1dc988bd115d8ba7f25ba53d89093dc8f
logickoder <chukwudumebiorazulike@gmail.com>
2023-07-14 11:46:10 +0100

    feat: initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,95 @@
+@file:Suppress("UnstableApiUsage","DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+    namespace = "dev.logickoder.jobfinder"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "dev.logickoder.jobfinder"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        // Flag to enable support for the new language APIs
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = rootProject.libs.versions.compose.compiler.get()
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    // for using some java 8 classes like LocalDate with older versions of android
+    coreLibraryDesugaring(libs.core.java8)
+
+    implementation(libs.core)
+    implementation(libs.core.appcompat)
+
+    // Material Design
+    implementation(libs.core.material)
+    implementation(libs.compose.material)
+    implementation(libs.compose.material.icons)
+
+    // Compose
+    implementation(platform(libs.compose))
+    implementation(libs.compose.activity)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.fonts)
+    implementation(libs.compose.ui.tooling.preview)
+    debugImplementation(libs.compose.ui.tooling)
+    androidTestImplementation(platform(libs.compose))
+    androidTestImplementation(libs.compose.ui.test.junit)
+    androidTestImplementation(libs.compose.ui.test.manifest)
+
+    // lifecycle
+    implementation(libs.lifecycle.runtime)
+    implementation(libs.lifecycle.viewmodel)
+    implementation(libs.lifecycle.viewmodel.compose)
+
+    // preferences datastore
+    implementation(libs.datastore)
+
+    // Kotlinx
+    implementation(libs.kotlin.immutable)
+    implementation(libs.kotlin.serialization)
+
+    // Napier
+    implementation(libs.napier)
+
+    testImplementation(libs.junit4)
+    androidTestImplementation(libs.expresso)
+    androidTestImplementation(libs.junit4.androidx)
+}
---
+++ app/src/main/java/dev/logickoder/jobfinder/app/theme/Type.kt
@@ -0,0 +1,43 @@
+package dev.logickoder.jobfinder.app.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.unit.sp
+import dev.logickoder.jobfinder.R
+
+val Typography: Typography
+    get() {
+        val provider = GoogleFont.Provider(
+            providerAuthority = "com.google.android.gms.fonts",
+            providerPackage = "com.google.android.gms",
+            certificates = R.array.com_google_android_gms_fonts_certs,
+        )
+
+        val fontFamily = FontFamily(
+            Font(googleFont = GoogleFont("Poppins"), fontProvider = provider)
+        )
+
+        val typography = Typography()
+
+        return typography.copy(
+            displayLarge = typography.displayLarge.copy(fontFamily = fontFamily),
+            displayMedium = typography.displayMedium.copy(fontFamily = fontFamily),
+            displaySmall = typography.displaySmall.copy(fontFamily = fontFamily),
+            headlineLarge = typography.headlineLarge.copy(fontFamily = fontFamily),
+            headlineMedium = typography.headlineMedium.copy(fontFamily = fontFamily),
+            headlineSmall = typography.headlineMedium.copy(fontFamily = fontFamily),
+            titleLarge = typography.titleLarge.copy(fontFamily = fontFamily),
+            titleMedium = typography.titleMedium.copy(fontFamily = fontFamily),
+            titleSmall = typography.titleSmall.copy(fontFamily = fontFamily),
+            bodyLarge = typography.bodyLarge.copy(fontFamily = fontFamily),
+            bodyMedium = typography.bodyMedium.copy(fontFamily = fontFamily),
+            bodySmall = typography.bodySmall.copy(fontFamily = fontFamily),
+            labelLarge = typography.labelLarge.copy(fontFamily = fontFamily),
+            labelMedium = typography.labelMedium.copy(fontFamily = fontFamily),
+            labelSmall = typography.labelSmall.copy(fontFamily = fontFamily),
+        )
+    }
