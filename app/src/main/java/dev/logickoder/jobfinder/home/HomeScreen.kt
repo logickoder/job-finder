5082d8b3b21c9e13cc1419ea794c31bc84ee23c6
logickoder <chukwudumebiorazulike@gmail.com>
2023-07-14 19:25:29 +0100

    feat: created the HomeBottomBar

--- app/src/main/java/dev/logickoder/jobfinder/home/HomeScreen.kt
+++ app/src/main/java/dev/logickoder/jobfinder/home/HomeScreen.kt
@@ -1,24 +1,43 @@
 package dev.logickoder.jobfinder.home
 
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import dev.logickoder.jobfinder.app.theme.JobFinderTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
 ) {
+    var selectedIndex by remember { mutableIntStateOf(0) }
+
     Scaffold(
         modifier = modifier,
         topBar = {
             HomeTopBar()
         },
-        content = {
+        content = { scaffoldPadding ->
+            Column(
+                modifier = Modifier.padding(scaffoldPadding),
+                content = {
 
+                }
+            )
+        },
+        bottomBar = {
+            HomeBottomBar(
+                selectedIndex = selectedIndex,
+                onItemSelected = {
+                    selectedIndex = it
+                }
+            )
         }
     )
 }
