7bf25463947caa213dc8f5b7bc96f788a2068762
logickoder <chukwudumebiorazulike@gmail.com>
2023-07-15 15:30:10 +0100

    feat: created the JobSearch widget

--- app/src/main/java/dev/logickoder/jobfinder/home/HomeScreen.kt
+++ app/src/main/java/dev/logickoder/jobfinder/home/HomeScreen.kt
@@ -2,15 +2,21 @@ package dev.logickoder.jobfinder.home
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.logickoder.jobfinder.app.theme.JobFinderTheme
+import dev.logickoder.jobfinder.app.theme.padding
+import dev.logickoder.jobfinder.app.theme.paddingSmall
 
 @Composable
 fun HomeScreen(
@@ -21,13 +27,31 @@ fun HomeScreen(
     Scaffold(
         modifier = modifier,
         topBar = {
-            HomeTopBar()
+            HomeTopBar(
+                modifier = Modifier.padding(
+                    horizontal = padding() - 16.dp,
+                )
+            )
         },
         content = { scaffoldPadding ->
             Column(
                 modifier = Modifier.padding(scaffoldPadding),
                 content = {
-
+                    Text(
+                        modifier = Modifier.padding(
+                            horizontal = padding(),
+                            vertical = paddingSmall(),
+                        ),
+                        text = "Your skill is required for many jobs",
+                        style = MaterialTheme.typography.headlineMedium.copy(
+                            fontWeight = FontWeight.W600,
+                        ),
+                    )
+                    JobSearch(
+                        modifier = Modifier.padding(
+                            horizontal = padding(),
+                        )
+                    )
                 }
             )
         },
---
+++ app/src/main/java/dev/logickoder/jobfinder/home/JobSearch.kt
@@ -0,0 +1,83 @@
+package dev.logickoder.jobfinder.home
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import dev.logickoder.jobfinder.R
+import dev.logickoder.jobfinder.app.theme.JobFinderTheme
+import dev.logickoder.jobfinder.app.theme.paddingSmall
+
+@Composable
+fun JobSearch(
+    modifier: Modifier = Modifier,
+) {
+    Layout(
+        modifier = modifier,
+        content = {
+            Image(
+                modifier = Modifier.fillMaxWidth(),
+                painter = painterResource(id = R.drawable.img_job_search),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+            )
+            Button(
+                onClick = {},
+                content = {
+                    Row(
+                        content = {
+                            Icon(
+                                modifier = Modifier.padding(end = paddingSmall() / 3),
+                                imageVector = Icons.Filled.Search,
+                                contentDescription = null,
+                            )
+                            Text(
+                                text = "Search any job...",
+                                style = MaterialTheme.typography.bodyLarge.copy(
+                                    fontWeight = FontWeight.W600,
+                                ),
+                                color = MaterialTheme.colorScheme.onPrimary,
+                            )
+                        }
+                    )
+                },
+            )
+        },
+        measurePolicy = { measurables, constraints ->
+            // Don't constrain child views further, measure them with given constraints
+            // Measure each child
+            val image = measurables[0].measure(constraints)
+            val button = measurables[1].measure(constraints)
+            // Set the size of the layout as big as it can
+            layout(image.width, image.height + button.height / 2) {
+                // place the image first on the screen
+                image.placeRelative(0, 0)
+                // place the button over the bottom center of the image
+                button.placeRelative(
+                    x = (image.width / 2) - (button.width / 2),
+                    y = image.height - button.height / 2,
+                )
+            }
+        },
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun JobSearchPreview() = JobFinderTheme {
+    JobSearch()
+}
