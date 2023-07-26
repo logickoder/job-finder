301d0b2639056ed0377418b7e6db6ada81a25732
logickoder <chukwudumebiorazulike@gmail.com>
2023-07-26 08:09:15 +0100

    feat: created the TopBar

--- app/src/main/java/dev/logickoder/jobfinder/jobdescription/JobDescriptionRoute.kt
+++ app/src/main/java/dev/logickoder/jobfinder/jobdescription/JobDescriptionRoute.kt
@@ -1,9 +1,11 @@
 package dev.logickoder.jobfinder.jobdescription
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.bumble.appyx.core.modality.BuildContext
 import com.bumble.appyx.core.node.Node
+import dev.logickoder.jobfinder.app.model.TestJobs
 
 class JobDescriptionRoute(
     buildContext: BuildContext,
@@ -12,6 +14,14 @@ class JobDescriptionRoute(
 
     @Composable
     override fun View(modifier: Modifier) {
+        val job = remember {
+            TestJobs.first { it.id == jobId }
+        }
 
+        JobDescriptionScreen(
+            job = job,
+            modifier = modifier,
+            onBack = ::navigateUp,
+        )
     }
 }
--- app/src/main/java/dev/logickoder/jobfinder/jobdescription/JobDescriptionScreen.kt
+++ app/src/main/java/dev/logickoder/jobfinder/jobdescription/JobDescriptionScreen.kt
@@ -1,8 +1,42 @@
 package dev.logickoder.jobfinder.jobdescription
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.BookmarkBorder
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.logickoder.jobfinder.app.model.Job
+import dev.logickoder.jobfinder.app.model.TestJobs
+import dev.logickoder.jobfinder.app.theme.JobFinderTheme
+import dev.logickoder.jobfinder.app.widget.TopBar
 
 @Composable
-fun JobDescriptionScreen() {
+fun JobDescriptionScreen(
+    job: Job,
+    modifier: Modifier = Modifier,
+    onBack: () -> Unit,
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopBar(
+                title = job.company,
+                onBack = onBack,
+                trailingIcon = Icons.Outlined.BookmarkBorder,
+            )
+        },
+        content = { scaffoldPadding ->
 
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun JobDescriptionScreenPreview() = JobFinderTheme {
+    JobDescriptionScreen(
+        job = TestJobs[0],
+        onBack = {},
+    )
 }
