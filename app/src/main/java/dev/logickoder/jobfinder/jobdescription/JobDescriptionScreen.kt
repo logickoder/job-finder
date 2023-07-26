a7171637897f52423c7a295804b647cdf35343e3
logickoder <chukwudumebiorazulike@gmail.com>
2023-07-26 08:14:25 +0100

    feat: added the apply now button to the bottom of JobDescriptionScreen

--- app/src/main/java/dev/logickoder/jobfinder/jobdescription/JobDescriptionScreen.kt
+++ app/src/main/java/dev/logickoder/jobfinder/jobdescription/JobDescriptionScreen.kt
@@ -1,14 +1,26 @@
 package dev.logickoder.jobfinder.jobdescription
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.BookmarkBorder
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.logickoder.jobfinder.R
 import dev.logickoder.jobfinder.app.model.Job
 import dev.logickoder.jobfinder.app.model.TestJobs
 import dev.logickoder.jobfinder.app.theme.JobFinderTheme
+import dev.logickoder.jobfinder.app.theme.padding
 import dev.logickoder.jobfinder.app.widget.TopBar
 
 @Composable
@@ -21,13 +33,33 @@ fun JobDescriptionScreen(
         modifier = modifier,
         topBar = {
             TopBar(
+                modifier = Modifier.padding(horizontal = padding() - 16.dp),
                 title = job.company,
                 onBack = onBack,
                 trailingIcon = Icons.Outlined.BookmarkBorder,
             )
         },
         content = { scaffoldPadding ->
+            Column(
+                modifier = Modifier
+                    .padding(padding())
+                    .padding(scaffoldPadding),
+                content = {
+                    Spacer(modifier = Modifier.weight(1f))
 
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = {},
+                        content = {
+                            Text(
+                                text = stringResource(R.string.apply_now),
+                                style = MaterialTheme.typography.bodySmall,
+                                fontWeight = FontWeight.W700,
+                            )
+                        }
+                    )
+                }
+            )
         }
     )
 }
