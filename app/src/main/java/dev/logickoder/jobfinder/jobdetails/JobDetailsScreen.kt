04676137c0a5b79b40229953b08cdb5ac393525d
logickoder <chukwudumebiorazulike@gmail.com>
2023-07-27 15:59:17 +0100

    feat: added the JobDetailItem

--- app/src/main/java/dev/logickoder/jobfinder/jobdetails/JobDetailsScreen.kt
+++ app/src/main/java/dev/logickoder/jobfinder/jobdetails/JobDetailsScreen.kt
@@ -1,8 +1,10 @@
 package dev.logickoder.jobfinder.jobdetails
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -16,6 +18,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,7 +32,9 @@ import dev.logickoder.jobfinder.app.model.TestJobs
 import dev.logickoder.jobfinder.app.theme.JobFinderTheme
 import dev.logickoder.jobfinder.app.theme.padding
 import dev.logickoder.jobfinder.app.theme.paddingSecondary
+import dev.logickoder.jobfinder.app.theme.paddingSmall
 import dev.logickoder.jobfinder.app.widget.TopBar
+import kotlinx.collections.immutable.persistentListOf
 
 @Composable
 fun JobDetailsScreen(
@@ -46,17 +53,18 @@ fun JobDetailsScreen(
             )
         },
         content = { scaffoldPadding ->
-            Column(
-                modifier = Modifier
-                    .padding(padding())
-                    .padding(scaffoldPadding),
+            val scrollState = rememberScrollState()
+            Box(
+                modifier = Modifier.padding(scaffoldPadding),
                 content = {
                     Column(
                         modifier = Modifier
-                            .verticalScroll(rememberScrollState())
-                            .fillMaxWidth(),
+                            .padding(bottom = padding() + paddingSecondary())
+                            .verticalScroll(scrollState)
+                            .padding(padding()),
                         horizontalAlignment = Alignment.CenterHorizontally,
                         content = {
+                            Spacer(modifier = Modifier.height(padding()))
                             AsyncImage(
                                 modifier = Modifier.size(70.dp),
 //                                modifier = Modifier.fillMaxSize(0.16f),
@@ -77,13 +85,43 @@ fun JobDetailsScreen(
                                 job = job
                             )
                             JobDetailsInfoToggle(modifier = Modifier.fillMaxWidth())
+                            Spacer(modifier = Modifier.height(padding()))
+                            JobDetailItem(
+                                title = stringResource(id = R.string.about_job),
+                                content = {
+                                    Text(
+                                        text = "Infosys Limited is an Indian multinational information technology company that provides business consulting, information technology and outsource services."
+                                    )
+                                }
+                            )
+                            Spacer(modifier = Modifier.height(paddingSmall()))
+                            JobDetailItem(
+                                title = stringResource(id = R.string.job_description),
+                                content = {
+                                    JobDetailBulletText(
+                                        texts = persistentListOf(
+                                            "We are looking for a C# developer to build software using languages, technologies of the .NET framework.",
+                                            "You will create applications from scratch, configure existing systems and provide user support.Must have Potential to design, develop program independently."
+                                        )
+                                    )
+                                }
+                            )
                         }
                     )
-
-                    Spacer(modifier = Modifier.weight(1f))
-
                     Button(
-                        modifier = Modifier.fillMaxWidth(),
+                        modifier = Modifier
+                            .padding(start = padding(), end = padding(), bottom = padding())
+                            .drawBehind {
+                                // adds a small shadow on top the text to show content is still available
+                                // and removes the shadow when the scroll has reached the last position
+                                if (scrollState.canScrollForward) translate(top = -20.dp.toPx()) {
+                                    drawRect(
+                                        Color.White.copy(alpha = 0.5f),
+                                    )
+                                }
+                            }
+                            .align(Alignment.BottomCenter)
+                            .fillMaxWidth(),
                         onClick = {},
                         content = {
                             Text(
